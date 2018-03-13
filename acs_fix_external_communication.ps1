223bc5b858579277c2d4b15f3882bccbaf54dfe8
Alexander Wahlsten <alexander@qmnet.se>
2018-03-13 08:26:27 +0100

    Update acs_fix_external_communication.ps1

--- acs_fix_external_communication.ps1
+++ acs_fix_external_communication.ps1
@@ -1,5 +1,5 @@
 Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\hns.psm1
-Import-Module -name C:\hns.psm1
+Import-Module -name C:\hns.psm1 -Verbose
 Stop-Service kubeproxy
 Stop-Service kubelet
 Get-HNSNetwork | ? Name -eq l2Bridge | Remove-HnsNetwork 
