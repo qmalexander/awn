14f36e701524022e1beebf0f1b026de0349befb0
Alexander Wahlsten <alexander@qmnet.se>
2018-03-19 09:58:03 +0100

    Update acs_fix_external_communication_docker_start.ps1

--- acs_fix_external_communication_docker_start.ps1
+++ acs_fix_external_communication_docker_start.ps1
@@ -2,8 +2,8 @@ Get-Service docker | Stop-Service -PassThru | Set-Service -StartupType manual
 $testPath = Test-Path C:\hns.psm1 -PathType Leaf;
 if($testPath  -eq $false){
   Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\hns.psm1
-  Import-Module -name C:\hns.psm1 -Verbose
 }
+Import-Module -name C:\hns.psm1 -Verbose
 Stop-Service kubeproxy
 Stop-Service kubelet
 Get-HNSNetwork | ? Name -eq l2Bridge | Remove-HnsNetwork 
