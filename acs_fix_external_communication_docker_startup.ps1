bda3eeb216de0cea0129ccc5239a604bd194c36c
Alexander Wahlsten <alexander@qmnet.se>
2018-03-19 14:12:22 +0100

    Update acs_fix_external_communication_docker_startup.ps1

--- acs_fix_external_communication_docker_startup.ps1
+++ acs_fix_external_communication_docker_startup.ps1
@@ -3,4 +3,4 @@ if(Test-Path C:\acs_fix_external_communication_docker_start.ps1 -PathType Leaf)
   Remove-Item C:\acs_fix_external_communication_docker_start.ps1
 }
 Start-BitsTransfer -Source https://raw.githubusercontent.com/qmalexander/awn/master/acs_fix_external_communication_docker_start.ps1 -Destination C:\acs_fix_external_communication_docker_start.ps1
-Start-Process -filepath C:\acs_fix_external_communication_docker_start.ps1
+C:\acs_fix_external_communication_docker_start.ps1
