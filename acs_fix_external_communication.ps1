ff7b3ed7426d9dbb3f2f231abd173e2c7b16242e
Alexander Wahlsten <alexander@qmnet.se>
2018-04-11 15:59:33 +0200

    Update acs_fix_external_communication.ps1

--- acs_fix_external_communication.ps1
+++ acs_fix_external_communication.ps1
@@ -1,8 +1,13 @@
-Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\hns.psm1
+$testPath = Test-Path C:\hns.psm1 -PathType Leaf;
+if($testPath  -eq $false){
+  Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\hns.psm1
+}
 Import-Module -name C:\hns.psm1 -Verbose
+Stop-Service docker
 Stop-Service kubeproxy
 Stop-Service kubelet
 Get-HNSNetwork | ? Name -eq l2Bridge | Remove-HnsNetwork 
 Get-HnsPolicyList | Remove-HnsPolicyList
+Start-Service docker
 Start-Service kubelet
 Start-Service kubeproxy
