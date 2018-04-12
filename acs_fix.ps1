785455d2de6441fe175411cb40234aabd6a9761e
Alexander Wahlsten <alexander@qmnet.se>
2018-04-12 12:51:53 +0200

    Update acs_fix.ps1

--- acs_fix.ps1
+++ acs_fix.ps1
@@ -10,27 +10,24 @@ else{
     Register-ScheduledJob -Trigger $trigger -FilePath $currentFile -Name TeleoptiFix    
 }
 
-
 if(Get-HotFix | Where-Object HotFixID -Match "KB4089848"){
     Write-Host 'Found KB4089848'
-}
+    $testPath = Test-Path C:\k\hns.psm1 -PathType Leaf;
+    if($testPath  -eq $false){
+        Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\k\hns.psm1
+    }
+    Import-Module -name C:\k\hns.psm1 -Verbose
+    Stop-Service docker
+    Stop-Service kubeproxy
+    Stop-Service kubelet
+    Get-HNSNetwork | ? Name -eq l2Bridge | Remove-HnsNetwork 
+    Get-HnsPolicyList | Remove-HnsPolicyList
+    Start-Service docker
+    Start-Service kubelet
+    Start-Service kubeproxy
+    }
 else {
     Write-Host 'Installing KB4089848'
     Invoke-WebRequest http://download.windowsupdate.com/d/msdownload/update/software/updt/2018/03/windows10.0-kb4089848-x64_db7c5aad31c520c6983a937c3d53170e84372b11.msu -Out c:\kb4089848.msu
     wusa.exe c:\kb4089848.msu /quiet /forcerestart
 }
-
-
-$testPath = Test-Path C:\k\hns.psm1 -PathType Leaf;
-if($testPath  -eq $false){
-  Start-BitsTransfer -Source https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1 -Destination C:\k\hns.psm1
-}
-Import-Module -name C:\k\hns.psm1 -Verbose
-Stop-Service docker
-Stop-Service kubeproxy
-Stop-Service kubelet
-Get-HNSNetwork | ? Name -eq l2Bridge | Remove-HnsNetwork 
-Get-HnsPolicyList | Remove-HnsPolicyList
-Start-Service docker
-Start-Service kubelet
-Start-Service kubeproxy
