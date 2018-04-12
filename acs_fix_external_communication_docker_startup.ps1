50b242a14b5641cba6da0e88ff6c493c52044cea
Alexander Wahlsten <alexander@qmnet.se>
2018-04-12 12:59:22 +0200

    Delete acs_fix_external_communication_docker_startup.ps1

--- acs_fix_external_communication_docker_startup.ps1
+++
@@ -1,7 +0,0 @@
-if(Test-Path C:\acs_fix_external_communication_docker_start.ps1 -PathType Leaf)
-{
-  Remove-Item C:\acs_fix_external_communication_docker_start.ps1
-}
-$random = Get-Random -Maximum 100
-Invoke-Expression "Start-BitsTransfer -Source https://raw.githubusercontent.com/qmalexander/awn/master/acs_fix_external_communication_docker_start.ps1?${random} -Destination C:\acs_fix_external_communication_docker_start.ps1"
-C:\acs_fix_external_communication_docker_start.ps1
